26ce7380de7b1cc5f826cb92ab72ca83eecd4548
Alex Jing <alex.jing@databricks.com>
2023-08-11 00:32:40 -0700

    benchmark script

--- benchmark.sh
+++ benchmark.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
+arg1="$1"
+arg2="$2"
+
 JAVA_HOME=/usr/lib/jvm/zulu8-ca-amd64/ ./gradlew build
 
-for ((i=1; i<=10; i++)); do
+for ((i=1; i<=$arg1; i++)); do
   echo "jdk8"
-  /usr/lib/jvm/zulu8-ca-amd64/bin/java -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main 1000000 | grep "Workload time"
+  /usr/lib/jvm/zulu8-ca-amd64/bin/java -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main $arg2 | grep "Workload time"
   echo "jdk17"
-  /usr/lib/jvm/zulu17-ca-amd64/bin/java -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main 1000000 | grep "Workload time"
+  /usr/lib/jvm/zulu17-ca-amd64/bin/java -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main $arg2 | grep "Workload time"
 done
