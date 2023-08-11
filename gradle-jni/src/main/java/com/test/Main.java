0b348458776f4ca77105192e10bb4aa2d526537c
Alex Jing <alex.jing@databricks.com>
2023-08-11 10:35:29 -0700

    add p99

--- gradle-jni/src/main/java/com/test/Main.java
+++ gradle-jni/src/main/java/com/test/Main.java
@@ -4,13 +4,15 @@ public class Main {
     public static void main(String[] args) throws InterruptedException {
         System.out.println("java version: " + System.getProperty("java.version"));
         System.out.println("jni version: " + JniWorker.version());
-        long iteration = Long.parseLong(args[0]);
+        long iteration = 1000;
+        if (args.length >0) {
+            iteration = Long.parseLong(args[0]);
+        }
         JniLogger logger = new JniLogger();
         JniWorker worker = JniWorker.createNewJniWorker();
         worker.setLogger(logger);
         worker.setIteration(iteration);
         worker.run();
-        assert logger.getIgnoredLogMessageCount() == iteration;
         System.out.println("number of ignored messages : " + logger.getIgnoredLogMessageCount());
 //        worker.start();
 //        Thread.sleep(2000);
