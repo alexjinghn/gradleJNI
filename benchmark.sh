9269a782362c6f01de2a8729ecc7bc12d56d8379
Ubuntu <alex.jing@ip-10-110-25-144.us-west-2.compute.internal>
2023-08-14 20:33:06 +0000

    benchmarking script

--- benchmark.sh
+++ benchmark.sh
@@ -3,8 +3,33 @@
 arg1="$1"
 arg2="$2"
 
-JAVA_HOME=/usr/lib/jvm/zulu8-ca-amd64/ ./gradlew build
 
+#JAVA_HOME_8=/usr/lib/jvm/java-8-openjdk-amd64
+#JAVA_HOME_17=/usr/lib/jvm/java-17-openjdk-amd64
+
+JAVA_HOME_8=/usr/lib/jvm/zulu8-ca-amd64
+JAVA_HOME_17=/usr/lib/jvm/zulu17-ca-amd64
+
+echo "-----------------------------------------------------------------"
+echo "JDK8 version:"
+echo "-----------------------------------------------------------------"
+$JAVA_HOME_8/bin/java -version
+
+
+echo "-----------------------------------------------------------------"
+echo "JDK17 version:"
+echo "-----------------------------------------------------------------"
+$JAVA_HOME_17/bin/java -version
+
+echo "-----------------------------------------------------------------"
+echo "building libraries....."
+echo "-----------------------------------------------------------------"
+
+JAVA_HOME=$JAVA_HOME_8 ./gradlew build
+
+echo "-----------------------------------------------------------------"
+echo "start benchmarking...."
+echo "-----------------------------------------------------------------"
 
 jdk8_time_taken_arr=()
 jdk8_mean_arr=()
@@ -26,7 +51,8 @@ jdk17_p90_arr=()
 for ((i=1; i<=$arg1; i++)); do
   echo "iteration $i"
 
-  output=$(/usr/lib/jvm/zulu8-ca-amd64/bin/java -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main $arg2 2>1)
+  output=$($JAVA_HOME_8/bin/java -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main $arg2 2>1)
+
   time_taken=$(echo "$output" | grep -oP '(?<=time taken \(microseconds\): )\d+')
   mean=$(echo "$output" | grep -oP '(?<=mean \(nanoseconds\): )\d+\.\d+')
   sd=$(echo "$output" | grep -oP '(?<=sd \(nanoseconds\): )\d+\.\d+')
@@ -45,7 +71,8 @@ for ((i=1; i<=$arg1; i++)); do
 
 
 
-  output=$(/usr/lib/jvm/zulu17-ca-amd64/bin/java -XX:+UseParallelGC -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main $arg2 2>1)
+  output=$($JAVA_HOME_17/bin/java -XX:+UseParallelGC -cp gradle-jni/build/libs/gradle-jni.jar com.test.Main $arg2 2>1)
+
   time_taken=$(echo "$output" | grep -oP '(?<=time taken \(microseconds\): )\d+')
   mean=$(echo "$output" | grep -oP '(?<=mean \(nanoseconds\): )\d+\.\d+')
   sd=$(echo "$output" | grep -oP '(?<=sd \(nanoseconds\): )\d+\.\d+')
@@ -71,3 +98,9 @@ for ((i = 0; i < ${#jdk8_time_taken_arr[@]}; i++)); do
     printf "jdk17 %-10s | %-15s | %-15s | %-15s | %-15s | %-15s | %-15s\n" "${jdk17_time_taken_arr[i]} μs" "${jdk17_mean_arr[i]} ns" "${jdk17_sd_arr[i]} ns" "${jdk17_p999_arr[i]} ns"  "${jdk17_p99_arr[i]} ns" "${jdk17_p95_arr[i]} ns" "${jdk17_p90_arr[i]} ns"
     echo "----------------------------------------"
 done
+
+
+echo "-----------------------------------------------------------------"
+echo "DONE!"
+echo "-----------------------------------------------------------------"
+
